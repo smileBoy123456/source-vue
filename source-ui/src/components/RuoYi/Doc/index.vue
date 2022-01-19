d4931cb5a1cb29b9cf42e8491c421b9fae1ed4c3
zy <1099264057@qq.com>
2022-01-19 09:40:10 +0800

    主页修改

--- source-ui/src/components/RuoYi/Doc/index.vue
+++ source-ui/src/components/RuoYi/Doc/index.vue
@@ -6,16 +6,16 @@
 
 <script>
 export default {
-  name: 'RuoYiDoc',
+  name: "RuoYiDoc",
   data() {
     return {
-      url: 'http://doc.ruoyi.vip/ruoyi-vue'
-    }
+      url: "8.136.213.245",
+    };
   },
   methods: {
     goto() {
-      window.open(this.url)
-    }
-  }
-}
+      window.open(this.url);
+    },
+  },
+};
 </script>
--- source-ui/src/components/RuoYi/Git/index.vue
+++ source-ui/src/components/RuoYi/Git/index.vue
@@ -6,16 +6,16 @@
 
 <script>
 export default {
-  name: 'RuoYiGit',
+  name: "RuoYiGit",
   data() {
     return {
-      url: 'https://gitee.com/y_project/RuoYi-Vue'
-    }
+      url: "https://gitee.com/open-source-byte/source-vue",
+    };
   },
   methods: {
     goto() {
-      window.open(this.url)
-    }
-  }
-}
+      window.open(this.url);
+    },
+  },
+};
 </script>
